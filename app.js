11426a106b60708f98faf677a5c2d9ab1a907cfa
Ruairidh MacArthur <74493680+roomacarthur@users.noreply.github.com>
2024-11-04 15:48:51 +0000

    Updates buttonHover for better readability

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-function buttonHover(){
-    let button = document.getElementsByClassName("answer");
-    for (let i = 0; i < button.length; i++) {
-        button[i].onmouseover=function(){
-            button[i].classList.add("mobileBtn");
-        };
-        button[i].onmouseleave=function(){
-            button[i].classList.remove("mobileBtn");
-        };
-    }
-};
+function buttonHover() {
+  let button = document.getElementsByClassName("answer");
+  for (let i = 0; i < button.length; i++) {
+    button[i].onmouseover = function () {
+      button[i].classList.add("mobileBtn");
+    };
+    button[i].onmouseleave = function () {
+      button[i].classList.remove("mobileBtn");
+    };
+  }
+}
+
 buttonHover();
